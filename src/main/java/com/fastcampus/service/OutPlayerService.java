9634eb9d45fb03193971021b1aa053d4e34dc784
YongHo Shin <gdtknight@naver.com>
2023-06-29 01:18:55 +0900

    OutPlayerService 구현

--- src/main/java/com/fastcampus/service/OutPlayerService.java
+++ src/main/java/com/fastcampus/service/OutPlayerService.java
@@ -1,6 +1,26 @@
 package com.fastcampus.service;
 
+import com.fastcampus.dao.OutPlayerDao;
+import com.fastcampus.dto.OutPlayerRespDto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OutPlayerService {}
+public class OutPlayerService {
+  private final OutPlayerDao outPlayerDao;
+
+  public OutPlayerService(@Autowired OutPlayerDao outPlayerDao) {
+    this.outPlayerDao = outPlayerDao;
+  }
+
+  public String registerOutPlayer(Integer playerId, String reason) {
+    outPlayerDao.insertOutPlayer(playerId, reason);
+
+    return "성공";
+  }
+
+  public List<OutPlayerRespDto> getOutPlayerList() {
+    return outPlayerDao.selectAll();
+  }
+}
