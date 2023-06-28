0b0172a47325aa34f0c84041a9cd4b5b61517780
YongHo Shin <gdtknight@naver.com>
2023-06-28 19:35:50 +0900

    StadiumDao 구현 완료

--- src/main/java/com/fastcampus/dao/StadiumDao.java
+++ src/main/java/com/fastcampus/dao/StadiumDao.java
@@ -1,3 +1,53 @@
 package com.fastcampus.dao;
 
-public class StadiumDao {}
+import com.fastcampus.db.DBConnection;
+import com.fastcampus.dto.StadiumDto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StadiumDao {
+  public void insertStadium(String name) {
+    String sql = "INSERT INTO stadium (name) VALUES (?)";
+
+    try (Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+      pstmt.setString(1, name);
+
+      int affectedRows = pstmt.executeUpdate();
+      if (affectedRows > 0) {
+        System.out.println("Stadium Registration Success");
+      } else {
+        System.out.println("Stadium Registration Failed");
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public List<StadiumDto> selectAll() {
+    List<StadiumDto> stadiumDtoList = new ArrayList<>();
+
+    String sql = "SELECT * FROM stadium;";
+
+    try (Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery()) {
+
+      while (rs.next()) {
+        stadiumDtoList.add(
+            StadiumDto.of(rs.getInt("id"), rs.getString("name"), rs.getTimestamp("created_at")));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return stadiumDtoList;
+  }
+}
