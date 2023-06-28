8e53290b379aafd481177ec13c945f3c745b0a7b
YongHo Shin <gdtknight@naver.com>
2023-06-28 19:43:56 +0900

    StadiumService 구현 완료

--- src/main/java/com/fastcampus/service/StadiumService.java
+++ src/main/java/com/fastcampus/service/StadiumService.java
@@ -1,6 +1,26 @@
 package com.fastcampus.service;
 
+import com.fastcampus.dao.StadiumDao;
+import com.fastcampus.dto.StadiumDto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StadiumService {}
+public class StadiumService {
+  StadiumDao stadiumDao;
+
+  public StadiumService(@Autowired StadiumDao stadiumDao) {
+    this.stadiumDao = stadiumDao;
+  }
+
+  public String registerStadium(String name) {
+    stadiumDao.insertStadium(name);
+
+    return "성공";
+  }
+
+  public List<StadiumDto> getStadiumList() {
+    return stadiumDao.selectAll();
+  }
+}
