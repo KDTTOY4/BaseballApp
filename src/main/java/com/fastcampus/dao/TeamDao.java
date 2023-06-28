7ed7c5ef69a52d53c1c48fe9de95a66730d11c00
YongHo Shin <gdtknight@naver.com>
2023-06-28 19:16:39 +0900

    TeamDao, TeamDto 파일 포맷 적용

--- src/main/java/com/fastcampus/dao/TeamDao.java
+++ src/main/java/com/fastcampus/dao/TeamDao.java
@@ -1,8 +1,7 @@
 package com.fastcampus.dao;
 
-import com.fastcampus.dbconnection.DBConnection;
+import com.fastcampus.db.DBConnection;
 import com.fastcampus.dto.TeamDto;
-import com.fastcampus.model.Team;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -13,54 +12,52 @@ import org.springframework.stereotype.Component;
 @Component
 public class TeamDao {
 
-    public void insertTeam(Integer stadiumId, String name) {
-        String sql = "INSERT INTO TEAM (stadium_id, name) VALUES (?, ?)";
+  public void insertTeam(Integer stadiumId, String name) {
+    String sql = "INSERT INTO TEAM (stadium_id, name) VALUES (?, ?)";
 
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+    try (Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            pstmt.setInt(1, stadiumId);
-            pstmt.setString(2, name);
+      pstmt.setInt(1, stadiumId);
+      pstmt.setString(2, name);
 
-            int affectedRows = pstmt.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("Team Registration Success");
-            } else {
-                System.out.println("Team Registration Failed");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+      int affectedRows = pstmt.executeUpdate();
+      if (affectedRows > 0) {
+        System.out.println("Team Registration Success");
+      } else {
+        System.out.println("Team Registration Failed");
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public List<TeamDto> selectAll() {
+    List<TeamDto> teamList = new ArrayList<>();
+
+    String sql =
+        "SELECT t.id, t.name, t.created_at, s.name as stadium_name FROM team t JOIN stadium s ON t.stadium_id = s.id";
+
+    try (Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery()) {
+
+      while (rs.next()) {
+        teamList.add(mapResultSetToTeamDto(rs));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    public List<TeamDto> selectAll() {
-        List<TeamDto> teamList = new ArrayList<>();
+    return teamList;
+  }
 
-        String sql = "SELECT t.id, t.name, t.created_at, s.name AS stadium_name " +
-                "FROM team t " +
-                "JOIN stadium s ON t.stadium_id = s.id";
+  private TeamDto mapResultSetToTeamDto(ResultSet rs) throws SQLException {
+    Integer id = rs.getInt("id");
+    String name = rs.getString("name");
+    Timestamp createdAt = rs.getTimestamp("created_at");
+    String stadiumName = rs.getString("stadium_name");
 
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
-
-            while (rs.next()) {
-                TeamDto teamDto = mapResultSetToTeamDto(rs);
-                teamList.add(teamDto);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return teamList;
-    }
-
-    private TeamDto mapResultSetToTeamDto(ResultSet rs) throws SQLException {
-        Integer id = rs.getInt("id");
-        String name = rs.getString("name");
-        Timestamp createdAt = rs.getTimestamp("created_at");
-        String stadiumName = rs.getString("stadium_name");
-
-        return new TeamDto(id, name, createdAt, stadiumName);
-    }
+    return new TeamDto(id, name, createdAt, stadiumName);
+  }
 }
--- src/main/java/com/fastcampus/dto/TeamDto.java
+++ src/main/java/com/fastcampus/dto/TeamDto.java
@@ -1,20 +1,23 @@
 package com.fastcampus.dto;
 
+import java.sql.Timestamp;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-import java.sql.Timestamp;
-@Getter@Setter
+@Getter
+@Setter
+@ToString
 public class TeamDto {
-    private Integer id;
-    private String name;
-    private Timestamp createdAt;
-    private String stadiumName;
+  private Integer id;
+  private String name;
+  private Timestamp createdAt;
+  private String stadiumName;
 
-    public TeamDto(Integer id, String name, Timestamp createdAt, String stadiumName) {
-        this.id = id;
-        this.name = name;
-        this.createdAt = createdAt;
-        this.stadiumName = stadiumName;
-    }
+  public TeamDto(Integer id, String name, Timestamp createdAt, String stadiumName) {
+    this.id = id;
+    this.name = name;
+    this.createdAt = createdAt;
+    this.stadiumName = stadiumName;
+  }
 }
