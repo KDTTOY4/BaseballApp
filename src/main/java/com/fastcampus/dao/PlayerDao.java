ccdb99a78d2433aca1f0c3119d737b94fa8338e2
ParkSeongUk <tjddnr633@naver.com>
2023-06-28 21:28:08 +0900

    playerDao 구현

--- src/main/java/com/fastcampus/dao/PlayerDao.java
+++ src/main/java/com/fastcampus/dao/PlayerDao.java
@@ -1,3 +1,68 @@
 package com.fastcampus.dao;
 
-public class PlayerDao {}
+import com.fastcampus.db.DBConnection;
+import com.fastcampus.dto.PlayerDto;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlayerDao {
+    public void insertPlayer(Integer teamId, String name, String position) {
+        String sql = "INSERT INTO player (team_id, name, position, created_at) VALUES (?, ?, ?, now())";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, teamId);
+            pstmt.setString(2, name);
+            pstmt.setString(3, position);
+
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("Player Registration Success");
+            } else {
+                System.out.println("Player Registration Failed");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<PlayerDto> selectAll(Integer teamId) {
+        List<PlayerDto> playerList = new ArrayList<>();
+
+        String sql =
+                "SELECT p.id, p.name, p.position, p.created_at, t.name as team_name "
+                        + "FROM player p JOIN team t ON p.team_id = t.id WHERE t.id = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, teamId);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                playerList.add(mapResultSetToPlayerDto(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return playerList;
+    }
+
+
+    private PlayerDto mapResultSetToPlayerDto(ResultSet rs) throws SQLException {
+        Integer id = rs.getInt("id");
+        String name = rs.getString("name");
+        String position = rs.getString("position");
+        Timestamp createdAt = rs.getTimestamp("created_at");
+        String teamName = rs.getString("team_name");
+
+        return new PlayerDto(id, name, position, createdAt, teamName);
+    }
+}
