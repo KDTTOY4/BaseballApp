b7c2f87b485af9bdfbd944442ed47be92a4b4fe6
ParkSeongUk <tjddnr633@naver.com>
2023-06-28 21:28:22 +0900

    playerService 구현

--- src/main/java/com/fastcampus/service/PlayerService.java
+++ src/main/java/com/fastcampus/service/PlayerService.java
@@ -1,6 +1,25 @@
 package com.fastcampus.service;
 
+import com.fastcampus.dao.PlayerDao;
+import com.fastcampus.dto.PlayerDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class PlayerService {}
+public class PlayerService {
+    private final PlayerDao playerDao;
+
+    public PlayerService(@Autowired PlayerDao playerDao) {
+        this.playerDao = playerDao;
+    }
+
+    public String registerPlayer(String name, String position, Integer teamId) {
+        playerDao.insertPlayer(teamId, name, position);
+        return "성공";
+    }
+
+    public List<PlayerDto> getPlayerList(Integer teamId) {return playerDao.selectAll(teamId);
+    }
+}
