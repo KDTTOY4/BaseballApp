df6b98722e82975aab9252bdfb1c371ba496e2d9
YongHo Shin <gdtknight@naver.com>
2023-06-29 15:25:16 +0900

    OutPlayer 모델 구현

--- src/main/java/com/fastcampus/model/OutPlayer.java
+++ src/main/java/com/fastcampus/model/OutPlayer.java
@@ -1,3 +1,15 @@
 package com.fastcampus.model;
 
-public class OutPlayer {}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
+public class OutPlayer {
+  Integer id;
+  Integer playerId;
+  String reason;
+  Timestamp createdAt;
+}
