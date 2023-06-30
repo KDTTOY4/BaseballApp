8a5873a7e73dcf9456307280b0950d62f7c6f5ed
ParkSeongUk <tjddnr633@naver.com>
2023-06-30 12:16:09 +0900

    TeamDao 예외처리 수정

--- src/main/java/com/fastcampus/dao/TeamDao.java
+++ src/main/java/com/fastcampus/dao/TeamDao.java
@@ -3,9 +3,13 @@ package com.fastcampus.dao;
 import com.fastcampus.db.DBConnection;
 import com.fastcampus.dto.PositionRespDto;
 import com.fastcampus.dto.TeamDto;
+import com.fastcampus.exceptions.BaseballAppException;
+import com.fastcampus.exceptions.code.AppErrorCode;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +19,7 @@ public class TeamDao {
     String sql = "INSERT INTO TEAM (stadium_id, name) VALUES (?, ?)";
 
     try (Connection conn = DBConnection.getConnection();
-        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+         PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
       pstmt.setInt(1, stadiumId);
       pstmt.setString(2, name);
@@ -27,7 +31,7 @@ public class TeamDao {
         System.out.println("Team Registration Failed");
       }
     } catch (SQLException e) {
-      e.printStackTrace();
+      handleSQLException(e);
     }
   }
 
@@ -35,17 +39,17 @@ public class TeamDao {
     List<TeamDto> teamList = new ArrayList<>();
 
     String sql =
-        "SELECT t.id, t.name, t.created_at, s.name as stadium_name FROM team t JOIN stadium s ON t.stadium_id = s.id";
+            "SELECT t.id, t.name, t.created_at, s.name as stadium_name FROM team t JOIN stadium s ON t.stadium_id = s.id";
 
     try (Connection conn = DBConnection.getConnection();
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        ResultSet rs = pstmt.executeQuery()) {
+         PreparedStatement pstmt = conn.prepareStatement(sql);
+         ResultSet rs = pstmt.executeQuery()) {
 
       while (rs.next()) {
         teamList.add(mapResultSetToTeamDto(rs));
       }
     } catch (SQLException e) {
-      e.printStackTrace();
+      handleSQLException(e);
     }
 
     return teamList;
@@ -67,16 +71,16 @@ public class TeamDao {
 
     for (TeamDto teamDto : teamDtoList) {
       sql.append(", ")
-          .append("max(case when t.name = '")
-          .append(teamDto.getName())
-          .append("' then p.name end) ")
-          .append(teamDto.getName());
+              .append("max(case when t.name = '")
+              .append(teamDto.getName())
+              .append("' then p.name end) ")
+              .append(teamDto.getName());
     }
     sql.append(" from team t right outer join player p on p.team_id = t.id group by p.position;");
 
     List<PositionRespDto> positionRespDtoList = new ArrayList<>();
     try (Connection conn = DBConnection.getConnection();
-        PreparedStatement pstmt = conn.prepareStatement(sql.toString())) {
+         PreparedStatement pstmt = conn.prepareStatement(sql.toString())) {
 
       ResultSet rs = pstmt.executeQuery();
 
@@ -85,17 +89,21 @@ public class TeamDao {
 
         for (TeamDto teamDto : teamDtoList) {
           positionRespDto
-              .getPlayerNameMapByTeam()
-              .put(teamDto.getName(), rs.getString(teamDto.getName()));
+                  .getPlayerNameMapByTeam()
+                  .put(teamDto.getName(), rs.getString(teamDto.getName()));
         }
 
         positionRespDtoList.add(positionRespDto);
       }
 
     } catch (SQLException e) {
-      e.printStackTrace();
+      handleSQLException(e);
     }
 
     return positionRespDtoList;
+  }
+
+  private void handleSQLException(SQLException e) {
+    throw new BaseballAppException(AppErrorCode.DB_EXCEPTION);
   }
 }
