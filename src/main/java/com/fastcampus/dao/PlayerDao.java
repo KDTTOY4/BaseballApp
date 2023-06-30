92ac6406c3f109e9b7d386798e911e315c185254
YongHo Shin <gdtknight@naver.com>
2023-06-30 14:15:12 +0900

    코드 포맷팅 변경 적용

--- src/main/java/com/fastcampus/dao/PlayerDao.java
+++ src/main/java/com/fastcampus/dao/PlayerDao.java
@@ -4,7 +4,6 @@ import com.fastcampus.db.DBConnection;
 import com.fastcampus.dto.PlayerDto;
 import com.fastcampus.exceptions.BaseballAppException;
 import com.fastcampus.exceptions.code.AppErrorCode;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,90 +11,89 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PlayerDao {
-    public void insertPlayer(Integer teamId, String name, String position) {
-        String sql = "INSERT INTO player (team_id, name, position, created_at) VALUES (?, ?, ?, now())";
+  public void insertPlayer(Integer teamId, String name, String position)
+      throws BaseballAppException {
+    String sql = "INSERT INTO player (team_id, name, position, created_at) VALUES (?, ?, ?, now())";
 
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+    try (Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            pstmt.setInt(1, teamId);
-            pstmt.setString(2, name);
-            pstmt.setString(3, position);
+      pstmt.setInt(1, teamId);
+      pstmt.setString(2, name);
+      pstmt.setString(3, position);
 
-            int affectedRows = pstmt.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("Player Registration Success");
-            } else {
-                System.out.println("Player Registration Failed");
-            }
-        } catch (SQLException e) {
-            throw new BaseballAppException(AppErrorCode.DB_EXCEPTION);
-        } catch (Exception e) {
-            throw new BaseballAppException(AppErrorCode.UNKNOWN_EXCEPTION);
-        }
+      int affectedRows = pstmt.executeUpdate();
+
+      if (affectedRows == 0) {
+        throw new SQLException();
+      }
+    } catch (SQLException e) {
+      throw new BaseballAppException(AppErrorCode.DB_EXCEPTION);
+    } catch (Exception e) {
+      throw new BaseballAppException(AppErrorCode.UNKNOWN_EXCEPTION);
+    }
+  }
+
+  public List<PlayerDto> selectAll(Integer teamId) throws BaseballAppException {
+    List<PlayerDto> playerList = new ArrayList<>();
+
+    String sql =
+        "SELECT p.id, p.name, p.position, p.created_at, t.name as team_name "
+            + "FROM player p JOIN team t ON p.team_id = t.id WHERE t.id = ?";
+
+    try (Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+      pstmt.setInt(1, teamId);
+
+      ResultSet rs = pstmt.executeQuery();
+
+      while (rs.next()) {
+        playerList.add(mapResultSetToPlayerDto(rs));
+      }
+    } catch (SQLException e) {
+      throw new BaseballAppException(AppErrorCode.DB_EXCEPTION);
+    } catch (Exception e) {
+      throw new BaseballAppException(AppErrorCode.UNKNOWN_EXCEPTION);
     }
 
-    public List<PlayerDto> selectAll(Integer teamId) {
-        List<PlayerDto> playerList = new ArrayList<>();
+    return playerList;
+  }
 
-        String sql =
-                "SELECT p.id, p.name, p.position, p.created_at, t.name as team_name "
-                        + "FROM player p JOIN team t ON p.team_id = t.id WHERE t.id = ?";
+  private PlayerDto mapResultSetToPlayerDto(ResultSet rs) throws SQLException {
+    Integer id = rs.getInt("id");
+    String name = rs.getString("name");
+    String position = rs.getString("position");
+    Timestamp createdAt = rs.getTimestamp("created_at");
+    String teamName = rs.getString("team_name");
 
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+    return new PlayerDto(id, name, position, createdAt, teamName);
+  }
 
-            pstmt.setInt(1, teamId);
+  public PlayerDto selectById(Integer playerId) throws BaseballAppException {
+    PlayerDto player = null;
+    String sql =
+        "SELECT p.id, p.name, p.position, p.created_at, t.name AS team_name "
+            + "FROM player p "
+            + "LEFT OUTER JOIN team t ON p.team_id = t.id "
+            + "WHERE p.id = ?";
 
-            ResultSet rs = pstmt.executeQuery();
+    try (Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            while (rs.next()) {
-                playerList.add(mapResultSetToPlayerDto(rs));
-            }
-        } catch (SQLException e) {
-            throw new BaseballAppException(AppErrorCode.DB_EXCEPTION);
-        } catch (Exception e) {
-            throw new BaseballAppException(AppErrorCode.UNKNOWN_EXCEPTION);
-        }
+      pstmt.setInt(1, playerId);
 
-        return playerList;
+      ResultSet rs = pstmt.executeQuery();
+
+      if (rs.next()) {
+        player = mapResultSetToPlayerDto(rs);
+      }
+    } catch (SQLException e) {
+      throw new BaseballAppException(AppErrorCode.DB_EXCEPTION);
+    } catch (Exception e) {
+      throw new BaseballAppException(AppErrorCode.UNKNOWN_EXCEPTION);
     }
 
-
-    private PlayerDto mapResultSetToPlayerDto(ResultSet rs) throws SQLException {
-        Integer id = rs.getInt("id");
-        String name = rs.getString("name");
-        String position = rs.getString("position");
-        Timestamp createdAt = rs.getTimestamp("created_at");
-        String teamName = rs.getString("team_name");
-
-        return new PlayerDto(id, name, position, createdAt, teamName);
-    }
-
-    public PlayerDto selectById(Integer playerId) {
-        PlayerDto player = null;
-        String sql =
-                "SELECT p.id, p.name, p.position, p.created_at, t.name AS team_name "
-                        + "FROM player p "
-                        + "LEFT OUTER JOIN team t ON p.team_id = t.id "
-                        + "WHERE p.id = ?";
-
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setInt(1, playerId);
-
-            ResultSet rs = pstmt.executeQuery();
-
-            if (rs.next()) {
-                player = mapResultSetToPlayerDto(rs);
-            }
-        } catch (SQLException e) {
-            throw new BaseballAppException(AppErrorCode.DB_EXCEPTION);
-        } catch (Exception e) {
-            throw new BaseballAppException(AppErrorCode.UNKNOWN_EXCEPTION);
-        }
-
-        return player;
-    }
+    return player;
+  }
 }
